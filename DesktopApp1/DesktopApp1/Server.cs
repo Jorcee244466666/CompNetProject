86c93178a11383cd976b255bb2a94dea6154870f
Jordan Goldstein <JordanGoldstein@gatech.edu>
2018-10-08 14:10:27 +0800

    Created Server Class

--- DesktopApp1/DesktopApp1/Server.cs
+++ DesktopApp1/DesktopApp1/Server.cs
@@ -3,11 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 
 namespace ChatClient
 {
     class Server
     {
+        //Private Instance Variables
+        private IPAddress hostAddress;
+        private TcpListener serverSocket;
+        private TcpClient clientSocket;
 
+        public Server(String address, int portNum)
+        {
+            hostAddress = IPAddress.Parse(address);
+            serverSocket = new TcpListener(hostAddress, portNum);
+            clientSocket = default(TcpClient);
+            clientSocket = serverSocket.AcceptTcpClient();
+        }
+        public Server()
+        {
+            hostAddress = IPAddress.Loopback;
+            serverSocket = new TcpListener(hostAddress, 8888);
+            clientSocket = default(TcpClient);
+            clientSocket = serverSocket.AcceptTcpClient();
+        }
     }
 }
